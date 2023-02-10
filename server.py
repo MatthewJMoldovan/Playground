4dbc0a45eb3c26281c9a694a3134297b30ac90df
Matthew J. Moldovan <matthewmoldovan@outlook.com>
2023-02-10 11:32:44 -0700

    Playground

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Welcome"
+
+@app.route("/play")
+def play():
+    return render_template("index.html", amount = 3, box_color = "rgb(159,197,248)")
+
+@app.route("/play/<int:boxes>")
+def play1(boxes):
+    return render_template("index.html", amount = boxes, box_color = "rgb(159,197,248)")
+
+@app.route("/play/<int:boxes>/<box_color>")
+def play2(boxes,box_color):
+    return render_template("index.html", amount = boxes, box_color = box_color)
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
